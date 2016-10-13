ecd869882be477a74a929751c1e1db8dcca42906
Eva Lieu <itsevalieu@gmail.com>
2016-10-13 09:37:02 -0700

    Added functions to reset and start game, as well as a checkLetter function. Still need to to if/else statement for if userGuess === letter in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,73 @@
-//Variables
+
+var wordBank = ["rome", "madrid", "tokyo", "london", "athens", "paris"];
 var wins = 0; //should increment
-var guesses = 5; //should decrement
+var totalGuesses = 8; //should decrement
+var arrayOfLettersGuessed = []; //userInput should be pushed to end of array
+var underscore = [];
 
-//Arrays that are not included in Objects
-var lettersGuessed = []; //userInput should be pushed to end of array
-document.write(" " + wins);
-
-
-//Objects
-var worldPlaces = {
-	rome: ["Colosseum", "Pantheon", "Vatican City", "Trevi Fountain", "Sistene Chapel", "Roman Forum"],
-	madrid: ["Plaza Mayor", "Retiro Park", "Temple of Debod", "El Rastro"],
-	tokyo: ["Akihabara", "Shinjuku", "Harajuku", "Shibuya"],
-	london: ["Big Ben", "London Eye", "Buckingham Palace", "Westminster Abbey", "Hyde Park", "Tower Bridge"],
-	athens: ["Parthenon", "Acropolis", "Olympic Stadium"],
-	paris: ["Eiffel Tower", "Notre Dame", "The Louvre", "Arc de Triomphe"],
-	//Array to contain all of worldPlaces' arrays ^above^; "this" refers to the object it is within;
-	cities: [this.rome, this.madrid, this.tokyo, this.london, this.athens, this.paris]
-}
 
 //Functions
-
-	//need to call array from worldPlaces object
-console.log(worldPlaces.cities.length);
-for(i=0; i<worldPlaces.cities.length; i++){
-	worldPlaces.cities[i];
+function resetGame(){
+	var totalGuesses =  8;
+	var lettersGuessed = [];
+	var underscore = [];
 }
 
-function word(x){}
+function startGame(){
+	alert("Please guess a letter.");
+	var cityWord = wordBank[Math.floor(Math.random() * word.length)];
+	var wordDiv = document.getElementById("hiddenWord");
+	checkLetter();
+	
 
-//Script Below
-//1. Must capture user's input key:
-	document.onkeyup = function(event){
-		//2. Defines userGuess and stores a char key input; converts to LowerCase:
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		if(userGuess != [A-Za-z]){}
+	wordDiv.innerHTML = cityWord;
+}
+
+function checkLetter(){
+	for(var u=0; u < word.length; u++){
+		var guessesLeft = document.getElementById("chances");
+		var lag = document.getElementById("showLettersGuessed");
+		//var arrayOfLettersGuessed = [];
+		arrayOfLettersGuessed.push(userGuess); 
+		
+		for(var a=0; a < arrayOfLettersGuessed.length; a++){
+			var letterGuessed = document.createElement("div");	
+			letterGuessed.innerHTML = arrayOfLettersGuessed[a];
+			lag.appendChild(letterGuessed);
 	}
+		if(userGuess === cityWord.charAt(u)){
+			 
+		}else{
+			totalGuesses--;
+			guessesLeft.innerHTML = totalGuesses;
+
+		}
+	}
+}
+
+/*-------------Script Begins-------------*/
+
+startGame();
+//1. Must capture user's input key:
+document.onkeyup = function(event){
+	//2. Defines userGuess and stores a char key input; converts to LowerCase:
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		
+
+}
+
+/*------------Script Ends--------------*/
+
+
+/*if(got word){
+	var wins = 0;
+	var totalWins = document.getElementById("noOfWins");
+	wins++;
+	totalWins.innerHTML = wins;
+}*/
 
 //for(i=0; i<word.length)
 //_.charAt(#); -> returns a character of a string, based on number position
 //_.charAt(_.length-1); = > shows last character in string
+
+
