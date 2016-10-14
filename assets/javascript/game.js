d11ce5afcd24e1ceb39b52bfdc6c1e9454d6502e
Eva Lieu <itsevalieu@gmail.com>
2016-10-13 17:51:23 -0700

    Working on logic for checking if letters are right. Not working rn. Deleted test.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,87 @@
 
 var wordBank = ["rome", "madrid", "tokyo", "london", "athens", "paris"];
+var cityWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 var wins = 0; //should increment
 var totalGuesses = 8; //should decrement
-var arrayOfLettersGuessed = []; //userInput should be pushed to end of array
+var userGuess;
+var lettersGuessed = []; //userInput should be pushed to end of array
 var underscore = [];
 
+
+
+console.log(cityWord);
 
 //Functions
 function resetGame(){
 	var totalGuesses =  8;
 	var lettersGuessed = [];
 	var underscore = [];
+	var index = 0;
+	var userGuess;
+	cityWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+	
+	document.getElementById("chances").textContent = totalGuesses;
+}
+
+document.getElementById("instructions").innerHTML = "Instructions: Press any key to get started!";
+
+//call startGame function to begin
+document.onkeyup = function(event){
+	startGame();
 }
 
 function startGame(){
-	alert("Please guess a letter.");
-	var cityWord = wordBank[Math.floor(Math.random() * word.length)];
-	var wordDiv = document.getElementById("hiddenWord");
-	checkLetter();
+	document.getElementById("instructions").innerHTML = "Instructions: Click a letter to guess!";
+	document.getElementById("noOfWins").textContent = wins;
+	document.getElementById("chances").textContent = totalGuesses;
+	document.getElementById("hiddenWord").textContent = underscore.join(" ");
+	document.getElementById("showLettersGuessed").textContent = lettersGuessed.join(" ");
+	//will pick up on userGuesses
+	document.onkeyup = function(event){
 	
-
-	wordDiv.innerHTML = cityWord;
-}
-
-function checkLetter(){
-	for(var u=0; u < word.length; u++){
-		var guessesLeft = document.getElementById("chances");
-		var lag = document.getElementById("showLettersGuessed");
-		//var arrayOfLettersGuessed = [];
-		arrayOfLettersGuessed.push(userGuess); 
-		
-		for(var a=0; a < arrayOfLettersGuessed.length; a++){
-			var letterGuessed = document.createElement("div");	
-			letterGuessed.innerHTML = arrayOfLettersGuessed[a];
-			lag.appendChild(letterGuessed);
-	}
-		if(userGuess === cityWord.charAt(u)){
-			 
-		}else{
-			totalGuesses--;
-			guessesLeft.innerHTML = totalGuesses;
-
-		}
-	}
-}
-
-/*-------------Script Begins-------------*/
-
-startGame();
-//1. Must capture user's input key:
-document.onkeyup = function(event){
 	//2. Defines userGuess and stores a char key input; converts to LowerCase:
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		
+	var userGuessLength = userGuess.length;
+	lettersGuessed.push(userGuess);
+	for(var h = 0; h < cityWord.length; h++){
+		underscore[h] = "_";
+		document.getElementById("hiddenWord").textContent = underscore.join(" ");
+	}
+	//For loop for allowing the player to keep playing until guesses run out:
+	for(var g = 1; g < totalGuesses; g++){
+		lettersGuessed[g]; //*NEED TO TEST RUN
+		document.getElementById("showLettersGuessed").textContent = lettersGuessed.join(" ");
+	}
 
+	//Logic for checking if true or not
+	for (var i = 0; i < userGuessLength; i++){
+		if(userGuess === cityWord.charAt(i)){
+			underscore[i] = cityWord.charAt(i);
+			console.log(userGuess);
+
+		} else if(userGuess !== cityWord.charAt(i)){
+			totalGuesses--;
+		}
+	}
+		document.getElementById("hiddenWord").textContent = underscore.join(" ");	
+document.getElementById("showLettersGuessed").textContent = lettersGuessed.join(" ");
+	if(totalGuesses === 0){
+		document.getElementById("instructions").innerHTML = "Out of guesses! You lost! Please click another key to reset the game!";
+		document.getElementById("hiddenWord").textContent = cityWord;
+		document.onkeyup = function(event) {
+			resetGame();
+			startGame();
+		}
+	}else if(underscore.join("") === cityWord) {
+		document.getElementById("instructions").innerHTML = "Wow! You win! Please click another key to reset the game!";
+		wins++;
+		//document.getElementById("noOfWins").textContent = wins;
+		document.onkeyup = function(event) {
+			resetGame();
+			startGame();
+		}
+
+	}
+
+	}
 }
-
-/*------------Script Ends--------------*/
-
-
-/*if(got word){
-	var wins = 0;
-	var totalWins = document.getElementById("noOfWins");
-	wins++;
-	totalWins.innerHTML = wins;
-}*/
-
-//for(i=0; i<word.length)
-//_.charAt(#); -> returns a character of a string, based on number position
-//_.charAt(_.length-1); = > shows last character in string
-
-
